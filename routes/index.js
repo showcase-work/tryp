d641759513abc82c5983ef06948292acb674b61d
Saurabh <saurabh.verma2892@gmail.com>
2017-03-08 12:38:28 -0600

    adding multiple languages to tryp

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+var json = require('../assets/languages/espaniol.json');
+var selected = "ES";
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    if(req.query.language == "en"){
+        json = require('../assets/languages/english.json');
+        selected="EN"
+    }
+    else
+    {
+        json = require('../assets/languages/espaniol.json');
+        selected = "ES";
+    }
+  res.render('index', { language: json, selected: selected});
 });
 
 module.exports = router;
