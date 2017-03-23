99df97b09dfd11c6807f8acf7ccd470478e1bbd9
Saurabh <saurabh.verma2892@gmail.com>
2017-03-23 17:09:24 -0600

    asd

--- routes/email.js
+++ routes/email.js
@@ -21,8 +21,8 @@ router.post('/', function(req, res, next) {
     if(req.body.email && req.body.firstName){
         to_email = new helper.Email(req.body.email);
 
-        subject = "TRYP Booking Request for req.body.firstName";
-        content = new helper.Content("text/html", "Hi "+req.body.firstName+", <br />"+" Your Booking query for <b>"+req.body.dateFrom+" to "+req.body.dateTo+"</b> is successfully recieved.<br /> We will respond back with a shortly. <br /><br />Thankyou for contacting TRYP.");
+        subject = "TRYP Booking Request for"+req.body.firstName;
+        content = new helper.Content("text/html", "Hi "+req.body.firstName+", <br />"+" Your Booking query for <b>"+req.body.dateFrom+" to "+req.body.dateTo+"</b> is successfully recieved.<br /> We will respond back with a shortly. <br /><br />Thank you for contacting TRYP.");
         mail = new helper.Mail(from_email, subject, to_email, content);
 
         var request = sg.emptyRequest({
@@ -35,7 +35,7 @@ router.post('/', function(req, res, next) {
         })
 
 
-        subject = "Customer: Booking Query";
+        subject = "Customer Booking Query";
         content = new helper.Content("text/html", "Hi, you have a query from "+req.body.firstName+"<br />Phone: "+req.body.phone+"<br />Email: "+req.body.email+"<br />Dats:"+req.body.dateFrom+"-"+req.body.dateTo);
         mail = new helper.Mail(from_email, subject, from_email, content);
 
